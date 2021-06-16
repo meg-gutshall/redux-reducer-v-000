c355c39a4b48420d67f28709f99c63b731ef7f93
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-15 22:15:53 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {
+        ...state,
+        friends: [...state.friends, action.friend],
+      };
+    case 'REMOVE_FRIEND':
+      return {
+        friends: state.friends.filter((f) => f.id !== action.id),
+      };
+    default:
+      return state;
+  }
 }
